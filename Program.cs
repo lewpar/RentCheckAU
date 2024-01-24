9773a07d584507c887ebfd59b93b6e393883fa6f
lewpar <lewis.o.parmenter@gmail.com>
2024-01-24 14:17:56 +1100

    Added support for searching for median rent for all units.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,74 @@
-﻿namespace RentCheckAU
+﻿using System.Text.RegularExpressions;
+
+namespace RentCheckAU
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static HttpClient? client;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool postCodeValid = false;
+            int postCode = 0;
+
+            while (!postCodeValid)
+            {
+                Console.WriteLine("Enter an Australian postcode.");
+                Console.Write("> ");
+                string? input = Console.ReadLine();
+
+                if (!int.TryParse(input, out var code))
+                {
+                    Console.WriteLine("Invalid postcode.");
+                    continue;
+                }
+
+                postCode = code;
+                postCodeValid = true;
+            }
+
+            client = new HttpClient();
+            string? content;
+
+            try
+            {
+                content = await client.GetStringAsync($"https://sqmresearch.com.au/weekly-rents.php?postcode={postCode}&t=1");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to fetch rent information for postcode '{postCode}'.");
+                Console.WriteLine($"Error: {ex.Message}");
+                await Main(args);
+                return;
+            }
+
+            if (content is null)
+            {
+                Console.WriteLine("Postcode content was null.");
+                await Main(args);
+                return;
+            }
+
+            content = content.Replace("\r", "");
+            content = content.Replace("\n", "");
+
+            var matches = Regex.Match(content, "<.*All Units.*?<div>(.*?)<\\/div>");
+
+            if (!matches.Success ||
+                matches.Groups.Count < 2)
+            {
+                Console.WriteLine("Failed to find any regex matches for unit pricing.");
+                return;
+            }
+
+            string? rentAvg = matches.Groups[1].Value.Trim();
+
+            Console.WriteLine($"The average rent for the postcode '{postCode}' is '{rentAvg}' AUD.");
+
+            Console.WriteLine("Press ENTER to search another postcode.");
+            Console.ReadLine();
+
+            await Main(args);
         }
     }
 }

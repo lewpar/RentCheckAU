c76deef656dfdd6771379607bfa39e7311932c00
lewpar <lewis.o.parmenter@gmail.com>
2024-01-24 14:16:57 +1100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RentCheckAU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
